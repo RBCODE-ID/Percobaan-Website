3ccb8755fb5a062824f8a95b71ff4e8adf4c728a
Adrian <adrianmaulanas023@gmail.com>
2023-04-24 22:21:48 +0700

    perocbaan

---
+++ Isi-Materi/Basic-Of-Photography/Pola-Diagonal/script.js
@@ -0,0 +1,15 @@
+// Toogle Class Active
+const navbarNav = document.querySelector(".navbar-nav ");
+
+// KETIKA HAMBURGER MENU DI KLICK
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+};
+
+// KLIK DILUAR SIDEBAR UNTUK MENGHILANGKAN NAV
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+    }
+});
